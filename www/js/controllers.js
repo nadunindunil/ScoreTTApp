86b002401c4e9cb6b1249c2de73d7455431ef8c0
nadunindunil <nadun1indunil@gmail.com>
2016-04-28 20:20:17 +0530

    score board edited

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,26 +1,49 @@
 
-tt.controller('ScoreCtrl', function($scope){
+tt.controller('ScoreCtrl', function($scope,$ionicPopup){
 
   
   $scope.name = "Nadun";
-  
+  $scope.set1player1 = 0;
+  $scope.set1player2 = 0;
+  $scope.set = 0;
+   
   $scope.score1 = 0;
   $scope.score2 = 0;
   $scope.player1 = "player1";    
   $scope.player2 = "player2";
-  
+  // $scope.set1player1 = $scope.score1;
+  // $scope.set1player2 = $scope.score2;
 
-  $scope.onSwipeUp = function(score){
+
+
+  $scope.onSwipeUp1 = function(score){
     score += 1;
+
+    $scope.set1player1 = score;
     return score;
   };
 
-  $scope.onSwipeDown = function(score){
+  $scope.onSwipeDown1 = function(score){
     if (score>0){ score -= 1; }
+    $scope.set1player1 = score;
+    return score;
+  };
+
+  $scope.onSwipeUp2 = function(score){
+    score += 1;
+
+    $scope.set1player2 = score;
+    return score;
+  };
+
+  $scope.onSwipeDown2 = function(score){
+    if (score>0){ score -= 1; }
+    $scope.set1player2 = score;
     return score;
   };
 
   $scope.onTouch = function() {
+
     var person = prompt("Please enter Player Name", "Player");
     
     if (person != null) {
@@ -30,6 +53,43 @@ tt.controller('ScoreCtrl', function($scope){
       return "null";
     }
   };
+
+  $scope.addScores = function(){
+    
+    //$scope.set1player1 = $scope.score1;
+    //$scope.$apply();
+
+  };
+
+  //new methods to control the score board
+  $scope.showConfirm = function() {
+   var confirmPopup = $ionicPopup.confirm({
+      title: 'Edit Colomn',
+      template: 'Values will be edited.',
+   });
+   confirmPopup.then(function(res) {
+      if (res) {
+         console.log('You clicked on "OK" button');
+      } else {
+         console.log('You clicked on "Cancel" button');
+      }
+   });
+  };
+
+  $scope.showPrompt = function() {
+   var promptPopup = $ionicPopup.prompt({
+      title: 'Change Name',
+      template: 'Enter the Name of Player'
+   });
+   promptPopup.then(function(res) {
+      if (res) {
+         console.log('Your input is ', res);
+      } else {
+         console.log('Please enter input');
+      }
+   });
+};
+  
 
   
 
